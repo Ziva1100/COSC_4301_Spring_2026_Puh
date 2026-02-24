4e3216e0b8b728dc0d59f903ca64d5a20c94005e
Ziva Puh <zivaviviane.puh@gmail.com>
2026-02-24 08:23:21 -0600

    Finished the CreatureRepository interface

--- src/main/java/org/example/neonarkintaketracker/repository/CreatureRepository.java
+++ src/main/java/org/example/neonarkintaketracker/repository/CreatureRepository.java
@@ -23,5 +23,17 @@ import org.example.neonarkintaketracker.entity.Creature;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface CreatureRepository {
+// simple CRUD with many default mathods
+public interface CreatureRepository extends JpaRepository<Creature, Long> {
+
+    // basic read functionality is automatic,
+    // does not need extra method declarations
+    // methods:
+    // save(entity)        -> insert or update a creature
+    // findById(id)        -> get one creature by primary key
+    // findAll()           -> get all creatures
+    // deleteById(id)      -> delete by primary key
+    // delete(entity)      -> delete by passing the entity itself
+
+    // also paging and sorting methods
 }
---
+++ src/main/java/org/example/neonarkintaketracker/service/CreatureService.java
@@ -0,0 +1,4 @@
+package org.example.neonarkintaketracker.service;
+
+public class CreatureService {
+}

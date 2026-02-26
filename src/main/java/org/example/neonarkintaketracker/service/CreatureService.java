1046e16101174035150b7dee98b21659047f1fa1
Ziva Puh <zivaviviane.puh@gmail.com>
2026-02-26 09:42:54 -0600

    added changed so CreatureService file

--- src/main/java/org/example/neonarkintaketracker/service/CreatureService.java
+++ src/main/java/org/example/neonarkintaketracker/service/CreatureService.java
@@ -1,4 +1,29 @@
+//***************************************************************
+//
+//  Developer:    Ziva Puh
+//
+//  Project #:    Project #2
+//
+//  File Name:    CreatureService.java
+//
+//  Course:       COSC 4301 Modern Programming
+//
+//  Due Date:     3/28/2026
+//
+//  Instructor:   Prof. Jon-Mikel Pearson
+//
+//  Description:  The class that handles business logic
+//  for the Creature Class
+//
+//***************************************************************
+
 package org.example.neonarkintaketracker.service;
 
+import org.example.neonarkintaketracker.entity.Creature;
+import org.example.neonarkintaketracker.repository.CreatureRepository;
+import org.springframework.stereotype.Service;
+
 public class CreatureService {
+
+    
 }

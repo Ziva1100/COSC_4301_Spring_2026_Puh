b7a110d83339cc00f3658a38fb0024eebe3b8795
Ziva Puh <zivaviviane.puh@gmail.com>
2026-04-01 11:43:54 -0500

    Added table creation for roles, clearances, and dimensions

---
+++ src/main/resources/db/init.sql
@@ -0,0 +1,55 @@
+----------------------------------------
+-- Developer: Ziva Puh
+--
+-- Project#: Project #3
+--
+-- File Name: init.sql
+--
+-- Course: COSC 4301 Modern Programming
+--
+-- Due Date: 4/18/2026
+--
+-- Instructor: Prof. Jon-Mikel Pearson
+--
+-- Description: Create a single sql file that creates
+-- the tables needed for Warden Menu functions,
+-- populates them and queries them to see the data
+-- needed for the menu.
+------------------------------------------
+
+-- STEP 0: DELETE ANY EXISTING TABLES
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS clearances;
+DROP TABLE IF EXISTS wardens;
+DROP TABLE IF EXISTS dimentions;
+DROP TABLE IF EXISTS status_log;
+DROP TABLE IF EXISTS certification_log;
+DROP TABLE IF EXISTS certifications;
+
+-- STEP 1: CREATING TABLES
+-- create roles table
+CREATE TABLE roles (
+    role_id INT GENERATED ALWAYS AS IDENTITY,
+    role_name VARCHAR(30) NOT NULL,
+    role_desc TEXT,
+
+    CONSTRAINT role_name_uk UNIQUE (role_name)
+);
+
+-- create table clearances
+CREATE TABLE clearances (
+    clearance_id INT GENERATED ALWAYS AS IDENTITY,
+    clearance_name VARCHAR(30) NOT NULL,
+    clearance_desc TEXT,
+
+    CONSTRAINT clearance_name_uk UNIQUE(clearance_name)
+);
+
+-- create table dimensions
+CREATE TABLE dimensions (
+    dimension_id INT GENERATED ALWAYS AS IDENTITY,
+    dimension_name VARCHAR(30) NOT NULL,
+    dimension_desc TEXT,
+
+    CONSTRAINT dimension_name_uk UNIQUE (dimension_name)
+);

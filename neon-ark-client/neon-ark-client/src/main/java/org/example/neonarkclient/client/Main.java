240cfb175de0f1fb7929e767caef179e21d80db2
Ziva Puh <zivaviviane.puh@gmail.com>
2026-05-04 12:00:09 -0500

    Added the front-end Java CLI server

---
+++ neon-ark-client/neon-ark-client/src/main/java/org/example/neonarkclient/client/Main.java
@@ -0,0 +1,42 @@
+//***************************************************************
+//
+//  Developer:    Ziva Puh
+//
+//  Project #:    Project #4
+//
+//  File Name:    Main.java
+//
+//  Course:       COSC 4301 Modern Programming
+//
+//  Due Date:     5/14/2026
+//
+//  Instructor:   Prof. Jon-Mikel Pearson
+//
+//  Description:  the runner class for the client. it startsup the menu
+//
+//***************************************************************
+
+package org.example.neonarkclient.client;
+
+
+public class Main {
+
+    //***************************************************************
+    //
+    //  Method:       main()
+    //
+    //  Description:  runs the client. If the mock flag is set to true
+    //  the main is run as a mock and does not connect ot the backend server
+    //
+    //  Parameters:   N/A
+    //
+    //  Returns:      N/A
+    //
+    //**************************************************************
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
